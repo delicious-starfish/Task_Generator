48bbc9bff0406b8974de22e6dc06739b2c2cdce9
delicious-starfish <18101894223@163.com>
2024-08-30 21:47:25 +0800

    test

--- main.py
+++ main.py
@@ -23,17 +23,17 @@ def generate_new_task(task_file_name):
         for audio in audio_list:
             f.write(json.dumps(audio,ensure_ascii=False) + '\n')
 
-    audio_list = get_audio_from('youtube')
-    print(len(audio_list))
-    with open(cfg['task_output_folder'] + task_file_name,'a',encoding = 'utf-8') as f:
-        for audio in audio_list:
-            f.write(json.dumps(audio,ensure_ascii=False) + '\n')
+    # audio_list = get_audio_from('youtube')
+    # print(len(audio_list))
+    # with open(cfg['task_output_folder'] + task_file_name,'a',encoding = 'utf-8') as f:
+    #     for audio in audio_list:
+    #         f.write(json.dumps(audio,ensure_ascii=False) + '\n')
 
-    audio_list = get_audio_from('archive')
-    print(len(audio_list))
-    with open(cfg['task_output_folder'] + task_file_name,'a',encoding = 'utf-8') as f:
-        for audio in audio_list:
-            f.write(json.dumps(audio,ensure_ascii=False) + '\n')
+    # audio_list = get_audio_from('archive')
+    # print(len(audio_list))
+    # with open(cfg['task_output_folder'] + task_file_name,'a',encoding = 'utf-8') as f:
+    #     for audio in audio_list:
+    #         f.write(json.dumps(audio,ensure_ascii=False) + '\n')
 
 def prepare_destination_folders():
     platforms = ['bilibili','youtube','archive']
