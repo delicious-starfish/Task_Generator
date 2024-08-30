95ed2101c881a0e266940bf29f5a8b7621981840
Starfish-Can <18101894223@163.com>
2024-08-30 11:27:35 +0800

    v0.2 移动处理过的音频

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 from Operate_File.file_read import read_json
+from Operate_File.file_build import build_dir
 
 cfg = read_json('config.json')
 
@@ -9,6 +10,18 @@ def move_previous_audio():
     if previous_task_file_name == 'Not Found':
         return
     processed_folder_dict = extract_folder_from_task(previous_task_file_name) #一个字典，key为文件夹名,value为平台名
+    prepare_destination_folders()
+    for taskname in processed_folder_dict.keys():
+        platform = processed_folder_dict[taskname]
+        move_task_folder(platform,taskname)
+
+def prepare_destination_folders():
+    platforms = ['bilibili','youtube','archive']
+    for platform in platforms:
+        path = os.path.join(cfg['move_destination'],platform)
+        build_dir(path)
+        
+        
 
 def extract_folder_from_task(task_file_name):
     with open(os.path.join(cfg['task_output_folder'], task_file_name),'r') as f:
@@ -24,8 +37,7 @@ def extract_folder_from_task(task_file_name):
             # 不同平台可能有相同的任务名
             task_dict[taskname] = platform
 
-
-    return []
+    return task_dict
     
 
 def find_previous_task():
@@ -41,6 +53,17 @@ def find_previous_task():
             latest_task_name = file
     return latest_task_name
 
+def move_task_folder(platform,taskname):
+    path = get_task_folder_path(platform,taskname)
+    dst = os.path.join(cfg["task_destination"],platform,taskname)
+
+    os.rename(path,dst)
+
+def get_task_folder_path(platform,taskname):
+    if platform == 'bilibili':
+        return os.path.join(cfg["bili_folder"],platform,taskname)
+    else:
+        return os.path.join(cfg['dl_file_folder'],platform,taskname)
 if __name__ == '__main__':
 
     move_previous_audio()
